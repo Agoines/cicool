85764a335462cd28f0990011a3f93a83b1dc7c2a
夜白 <yb3412436097@gmail.com>
2022-08-29 17:24:14 +0800

    修改主页

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,21 +1,85 @@
-// app.ts
+const domain = 'https://apitest.nijigen.fun/cicool'
+
+let isLogin = false
+let token = ''
+let userId = ''
+let bookId = ''
+
+let nickname = ''
+let avatarPic = ''
+
+let getAvatarPic = false
+let getNickname = false
+
+function getToken() {
+    return 'TOKEN=' + token
+}
+
+function getUserId() {
+    return userId
+}
+
 App({
     // 对应的例名
-    globalData: {
-        domain: 'https://apitest.nijigen.fun/cicool',
-        isLogin: false
+    globalData: {},
+
+    onLaunch: function () {
+        wx.login({
+                success: function (res) {
+                    //发送请求
+                    wx.request({
+                        method: "POST",
+                        url: domain + '/user/login',
+
+                        data: {
+                            code: res.code
+                        },
+
+                        header: {
+                            'content-type': 'application/json',
+                        },
+                        // 调用成功口
+                        success(r) {
+                            const res = r.data
+                            const data = res.data
+                            console.log(data)
+                            // 判断返回值
+                            if (res.errcode === 0) {
+                                // 存储对应的 信息
+                                try {
+                                    userId = data.id
+                                    token = data.token
+                                    bookId = data.bookId
+                                    nickname = data.nickName
+                                    avatarPic = data.avatarPic
+                                } catch (e) {
+                                    console.log(e)
+                                }
+                                isLogin = true
+                            }
+                        },
+                    })
+                }
+            }
+        )
     },
 
-    onLaunch() {
-
+    getNickname() {
+        console.log('getNickname')
+        if (nickname === '') {
+            return '词酷用户#8848'
+        } else {
+            return nickname
+        }
     },
 
-    emptyToken() {
-        return wx.getStorageSync('token') === ''
-    },
-
-    getToken() {
-        return 'TOKEN=' + wx.getStorageSync('token')
+    getAvatarPic() {
+        console.log('getAvatarPic')
+        if (avatarPic === '') {
+            return "https://api.multiavatar.com/" + getUserId() + "svg"
+        } else {
+            return avatarPic
+        }
     }
 })
 
